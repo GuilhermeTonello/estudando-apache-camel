1f7f477ea3bb28eb091a3b2e5f3b1cf3c082de68
Guilherme C. Tonello <guitonello@outlook.com.br>
2022-02-07 16:20:43 -0300

    Adicionando restConfiguration()

--- spring-camel/cpf-validator-api/src/main/java/gui/springcamel/cpfvalidatorapi/routes/ValidarCpfRoute.java
+++ spring-camel/cpf-validator-api/src/main/java/gui/springcamel/cpfvalidatorapi/routes/ValidarCpfRoute.java
@@ -1,7 +1,9 @@
 package gui.springcamel.cpfvalidatorapi.routes;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import gui.springcamel.cpfvalidatorapi.services.ValidarCpfService;
@@ -12,18 +14,24 @@ public class ValidarCpfRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		
+		restConfiguration()
+			.component("servlet")
+			.host("localhost")
+			.port(8081)
+			.bindingMode(RestBindingMode.json);
+		
 		rest("/validarCpf")
 			.get()
+				.produces(MediaType.APPLICATION_JSON_VALUE)
 				.param()
 					.name("cpf")
 					.required(true)
 					.type(RestParamType.query)
 				.endParam()
 				.route()
+					.routeId("route-validar-cpf")
 				.log("Validando cpf ${header.cpf}")
 				.bean(ValidarCpfService.class, "cpfValido(${header.cpf})")
-				.marshal()
-					.json()
 		.endRest();
 		
 	}
